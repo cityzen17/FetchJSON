a5147cc7c2f2a9a6f919170ef0e6fbef0f63734e
Cityzen10 <p.cityzen17@gmail.com>
2018-05-16 09:49:35 +0545

    Listview Display of JSON data

--- app/src/main/java/com/example/cityzen10/naxaassignment/MainActivity.java
+++ app/src/main/java/com/example/cityzen10/naxaassignment/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.cityzen10.naxaassignment;
 
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.ListView;
 
 import org.json.JSONArray;
@@ -10,7 +12,9 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
@@ -66,7 +70,6 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-
     private static String readURL(String theUrl) {
         StringBuilder content = new StringBuilder();
         try {
@@ -86,5 +89,16 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
         return content.toString();
+    }
+
+    private void writeToFile(String data) {
+        try {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(openFileOutput("config.txt", Context.MODE_PRIVATE));
+            outputStreamWriter.write(data);
+            outputStreamWriter.close();
+        }
+        catch (IOException e) {
+            Log.e("Exception", "File write failed: " + e.toString());
+        }
     }
 }
